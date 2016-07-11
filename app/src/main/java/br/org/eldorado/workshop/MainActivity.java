63ce3fd9bee19f969952ac7a4da211fe916dc4aa
Gustavo Luiz Kieling (MOTO) <gustavoknz@gmail.com>
2016-07-11 18:06:48 -0300

    Service added

--- app/src/main/java/br/org/eldorado/workshop/MainActivity.java
+++ app/src/main/java/br/org/eldorado/workshop/MainActivity.java
@@ -3,10 +3,13 @@ package br.org.eldorado.workshop;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends Activity {
+    private static final String TAG = "MainActivity";
+    private Intent serviceIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +25,19 @@ public class MainActivity extends Activity {
         in.putExtra("name", nameTyped);
 
         startActivity(in);
+    }
+
+    public void startServiceTapped(View view) {
+        serviceIntent = new Intent(getBaseContext(), ExampleService.class);
+        startService(serviceIntent);
+    }
+
+    public void stopServiceTapped(View view) {
+        if (serviceIntent != null) {
+            stopService(serviceIntent);
+            serviceIntent = null;
+        } else {
+            Log.d(TAG, "Service not started");
+        }
     }
 }
