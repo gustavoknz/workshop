3d27720084e1d44cdac8982c77b7eecd2b87fe34
Mauricio Klaus <mauricio.klaus@gmail.com>
2016-08-05 10:28:28 -0300

    apagar

--- app/src/main/java/br/org/eldorado/workshop/Constants.java
+++
@@ -1,6 +0,0 @@
-package br.org.eldorado.workshop;
-
-public class Constants {
-    public static final String SHARED_PREFERENCES_KEY = "Workshop";
-    public static final String SHARED_PREFERENCES_NAME_KEY = "name";
-}
