f3f5e10aba6332985a63108d99497034fbb64482
Gustavo Kieling <gustavoknz@gmail.com>
2016-08-16 13:39:19 -0300

    Facebook example added

---
+++ Face/app/src/main/java/br/org/eldorado/face/MainActivity.java
@@ -0,0 +1,60 @@
+package br.org.eldorado.face;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+        setContentView(R.layout.activity_main);
+
+        callbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "onSuccess ...");
+
+                Profile profile = Profile.getCurrentProfile();
+                ProfilePictureView profilePictureView;
+                profilePictureView = (ProfilePictureView) findViewById(R.id.image);
+                profilePictureView.setProfileId(profile.getId());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "onCancel ...");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "onError ...");
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+}
